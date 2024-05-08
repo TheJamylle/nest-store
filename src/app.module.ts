79fe6d752bba8911f77fa60375817522c194fff4
Jamylle <jamyllemagal@gmail.com>
2024-05-08 08:16:16 -0300

    feat: Implements docker db and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ProductModule } from './products/product.module';
+import { ProductModule } from './product/product.module';
 import { UserRepository } from './user/user.repository';
 import { UserExistsValidator } from './validation/user-exists.validator';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   providers: [UserRepository, UserExistsValidator],
 })
 export class AppModule {}
---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'db_store',
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
