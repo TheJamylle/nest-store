fcd3cab3486c1af5eb89e5a7e2d4f918eec643d3
Jamylle <jamyllemagal@gmail.com>
2024-05-09 02:12:08 -0300

    feat: Cleaning DTO

--- src/user/dto/UpdateUser.dto.ts
+++ src/user/dto/UpdateUser.dto.ts
@@ -1,17 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { EmailIsUnique } from '../validation/email-is-unique.validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDTO } from './CreateUser.dto';
 
-export class UpdateUserDTO {
-  @IsNotEmpty({ message: 'Name cannot be empty' })
-  @IsOptional()
-  name: string;
-
-  @IsEmail(undefined, { message: 'Invalid email' })
-  @EmailIsUnique({ message: 'Email already registered' })
-  @IsOptional()
-  email: string;
-
-  @MinLength(6, { message: 'Password must have at least 6 length' })
-  @IsOptional()
-  password: string;
-}
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {}
